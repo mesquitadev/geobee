518e2d4778572f01a1be7973205ae1b37567b79a
Victor Mesquita <mesquitadev@gmail.com>
2024-08-21 07:17:17 -0300

    send gitlab files

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,7 +5,9 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="relative min-h-screen  lg:grid lg:grid-cols-app">
       <Sidebar />
-      <main className="max-w-screen lg:col-start-2 lg:w-auto ">{children}</main>
+      <main className="w-screen h-screen lg:col-start-2 lg:w-auto">
+        {children}
+      </main>
     </div>
   )
 }
