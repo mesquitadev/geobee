c9c5184b6660f5ffed8cfaaabbb6daa2c939c190
Paulo Victor Costa Mesquita Pinto <paulo.mesquita@grupomateus.com>
2025-08-25 23:43:44 -0300

    :rocket: - refatorando projeto

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import viteCompression from 'vite-plugin-compression'
 export default defineConfig({
   plugins: [
     react(),
-    // Compressão de ativos para melhor performance
     viteCompression({
       algorithm: 'gzip',
       ext: '.gz',
@@ -16,29 +15,5 @@ export default defineConfig({
     port: 3000,
     open: true, // Abre no navegador automaticamente
     host: true, // Permite acessar via IP local
-  },
-  build: {
-    cssCodeSplit: true,
-    sourcemap: false,
-    chunkSizeWarningLimit: 1000, // Limite para alerta de chunk grande
-    rollupOptions: {
-      output: {
-        // Gera chunks apenas para pacotes principais
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendors'
-            }
-          }
-        },
-      },
-    },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 })
